09aa567ff44e739855120763280f0ac0d9aec881
Purushottam mishra <78618623+Purushottam67@users.noreply.github.com>
2023-05-09 10:34:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './components/molecules/nav';
+import Hero from './components/section/hero';
+function App() {
+  return (
+    <div className="App">
+      
+      <NavBar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
